f8e6d8f1632fbfc3b4deedc2ef6b0d99012a1832
ajay426neo <ajay426neo@gmail.com>
2017-09-30 13:32:22 -0400

    Tweet
    Added header file for Tweet class.

---
+++ Tweet.h
@@ -0,0 +1,22 @@
+#ifndef Tweet_h_
+#define Tweet_h_
+
+#include "User.h"
+#include <time.h> 
+
+
+class Tweet {
+public:
+	Tweet(std::string &userName, std::string &message, time_t &timeStamp);
+	~Tweet() { 
+  }
+// ACCESSORS
+	std::string getusername();
+	std::string getmessage();
+	time_t gettimestamp();
+private: // REPRESENTATION (member variables)
+	std::string userName;
+	std::string message;
+	time_t timeStamp;
+};
+#endif
